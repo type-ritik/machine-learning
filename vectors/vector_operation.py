44680b3a6e78bc53b3eecfd46d9ddd64ec8ca30f
rksvw <ritik.vw98@gmail.com>
2025-11-09 07:05:21 +0530

    add vector opt: mag, dot pro, dis, hada and outer pro

--- vectors/vector_operation.py
+++ vectors/vector_operation.py
@@ -68,4 +68,58 @@ def vec_brc():
     print(v.T + w.T)  # v : row vector, w : col vector
 
 
-vec_brc()
+def mag_uni_vec():
+    """Vector Magnitude"""
+    print("\nVector Magnitude:\n")
+    v = np.array([1, 2, 3, 7, 8, 9])
+    v_dim = len(v)  # math dimensionality
+    v_mag = np.linalg.norm(v)  # math magnitude, length, or norm
+    print(f"Vector Dimention: {v_dim}")
+    print(f"Vector norm: {v_mag}")
+
+
+def vec_dot_pro():
+    """Vector Dot Product"""
+    print("\nVector dot product:\n")
+    v = np.array([1, 2, 3, 4])
+    w = np.array([5, 6, 7, 8])
+    s = -1  # Scalar
+    dot = np.dot(v, w)
+    dot_scale = np.dot(s * v, w)
+    print(dot)
+    print(dot_scale)
+
+
+def dot_pro_dis():
+    """Dot Product Distributive"""
+    print("\nDot product distributive:\n")
+    a = np.array([0, 1, 2])
+    b = np.array([3, 5, 8])
+    c = np.array([13, 21, 34])
+
+    # the dot product is distributive
+    res1 = np.dot(a, b + c)
+    res2 = np.dot(a, b) + np.dot(a, c)
+
+    print(res1)
+    print(res2)
+
+
+def hadamard_mult():
+    """Hadamard Multiplication"""
+    print("\nHadamard multiplication:\n")
+    a = np.array([5, 4, 8, 2])
+    b = np.array([1, 0, 0.5, -1])
+    hadamard = a * b
+    print(hadamard)
+
+
+def out_pro():
+    """Outer Product Multiplication"""
+    print("\nOuter product multiplication:\n")
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5]).T
+    print(np.outer(a, b))
+
+
+out_pro()
